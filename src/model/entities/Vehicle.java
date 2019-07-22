fca53a3308e34b325e5264369020fcc864b38981
Felipe Salles <prof.felipe.salles@gmail.com>
2019-07-22 10:07:10 -0300

    sem interface

--- src/model/entities/Vehicle.java
+++ src/model/entities/Vehicle.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.entities;
 
-/**
- *
- * @author SEDU
- */
 public class Vehicle {
+
+    private String model;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+    
     
 }
